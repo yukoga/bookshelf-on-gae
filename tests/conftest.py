6cfe107f28cdf7bce6874dbde2428f311a1ad835
yukoga <yukoga@gmail.com>
2016-07-04 19:29:40 +0900

    slight fix on test code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,11 @@
 # *-* coding: utf-8 -*-
 
-from flask import url_for
 import bookshelf
 import config
 import pytest
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def app():
     app = bookshelf.create_app(config)
     return app
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,19 +1,17 @@
 # *-* coding: utf-8 -*-
 
-import config
 from flask import url_for
-from tests.conftest import *
 
 
 def test_app(app):
     assert app
 
 
-def test_config(client):
-    assert config.JSON_AS_ASCII == False
-    assert config.PROJECT_ID == 'gcp-jp'
-    assert config.DATA_BACKEND == 'datastore'
-    assert config.SECRET_KEY == 'secret'
+def test_config(client, config):
+    assert config['JSON_AS_ASCII'] == False
+    assert config['PROJECT_ID'] == 'gcp-jp'
+    assert config['DATA_BACKEND'] == 'datastore'
+    assert config['SECRET_KEY'] == 'secret'
 
 
 def test_list(client):
