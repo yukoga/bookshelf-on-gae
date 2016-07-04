d09fdcc46196a54d325b47216da0fed34cf63186
yukoga <yukoga@gmail.com>
2016-07-04 12:13:04 +0900

    Adding test case for api.list and api.get.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,7 @@
 # *-* coding: utf-8 -*-
 
 import config
+from flask import url_for
 from tests.conftest import *
 
 
@@ -13,3 +14,16 @@ def test_config(client):
     assert config.PROJECT_ID == 'gcp-jp'
     assert config.DATA_BACKEND == 'datastore'
     assert config.SECRET_KEY == 'secret'
+
+
+def test_list(client):
+    r = client.get(url_for('api.list'))
+    assert r.status_code == 200
+    assert 'Google Analytics' in r.data.decode('utf-8')
+    assert 'Goggle Analytics' not in r.data.decode('utf-8')
+
+
+def test_get(client):
+    r = client.get(url_for('api.get', id='5668600916475904'))
+    assert r.status_code == 200
+    assert '追加した本'.decode('utf-8') in r.data.decode('utf-8')
