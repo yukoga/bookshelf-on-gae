82254f6dc9e4ea26ae771b75bcfac88f68a9467b
yukoga <yukoga@gmail.com>
2016-07-07 14:19:17 +0900

    fix environmental change for PyCharm env.

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+
+current = os.path.abspath(os.path.dirname(__file__))
+path_dir = sys.path
+
+if current not in path_dir:
+    sys.path.insert(0, current)
+
+for path in sys.path:
+    print(path)
