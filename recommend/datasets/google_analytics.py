3824edd45f98eb937074460b66224910b4e81965
yukoga <yukoga@gmail.com>
2016-07-29 01:57:59 +0900

    Implemented Google Analytics datasets interface and its test.

--- recommend/datasets/google_analytics.py
+++ recommend/datasets/google_analytics.py
@@ -14,10 +14,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 from .abstract_dataset import AbstractDataset
 import httplib2
-from oauth2client import client
-from oauth2client import file
-from oauth2client import tools
-from setuptools.package_index import Credential
+from apiclient.errors import HttpError
+# from oauth2client import client
+# from oauth2client import file
+# from oauth2client import tools
+# from setuptools.package_index import Credential
 
 
 class GoogleAnalytics(AbstractDataset):
@@ -32,10 +33,10 @@ class GoogleAnalytics(AbstractDataset):
         '''
         # TODO: exception handling for required parameters.
         self.api_name = params['api_name']
-        self.api_version = params["api_version"]
-        self.scope = params["scope"]
-        self.key_file = params["key_file_location"]
-        self.service_account_email = params["service_account_email"]
+        self.api_version = params['api_version']
+        self.scope = params['scope']
+        self.key_file = params['key_file_location']
+        self.service_account_email = params['service_account_email']
 
         credentials = ServiceAccountCredentials.from_p12_keyfile(
             self.service_account_email, self.key_file, scopes=self.scope)
@@ -46,7 +47,50 @@ class GoogleAnalytics(AbstractDataset):
         self.target = None
 
     def fetch_data(self, query=None):
-        pass
+        if self.api_version is 'v4':
+            query = self._build_v4_query(query)
+        elif self.api_version is 'v3':
+            query = self._build_v3_query(query)
+        else:
+            raise TypeError('api version must be v3 or v4.')
+        self.total = self.fetch_table_data(query, self.service)
 
-    def fetch_table_data(self, table_name=None, file_name=None):
-        pass
+    def fetch_table_data(self, query=None, service=None):
+        try:
+            v4_response = service.reports().batchGet(body=query).execute()
+        except HttpError as error:
+            print('API error. There was an API error : %s : %s' %
+                  (error.resp.status, error._get_reason()))
+        rows = v4_response['reports'][0]['data']['rows']
+        headers = v4_response['reports'][0]['columnHeader']
+        index_col = []
+        if 'dimensions' in headers:
+            index_col = headers['dimensions']
+        index_col.extend([v.get('name') for v in headers[
+            'metricHeader']['metricHeaderEntries']])
+        index_col = [re.sub(r'ga:(.*)', r'\1', v) for v in index_col]
+        _table_data = []
+        _metrics = [v.get('metrics')[0].get('values') for v in rows]
+        if 'dimensions' in rows[0]:
+            _table_data = [v.get('dimensions') for v in rows]
+            _ = [u.extend(v) for u, v in zip(_table_data, _metrics)]
+        else:
+            _table_data = _metrics
+        return pd.DataFrame(_table_data, columns=index_col)
+
+    def _build_v3_query(self, params):
+        if not isinstance(params, dict):
+            raise TypeError('query parameters must be dictionary.')
+        required_parameters = ['metrics', 'ids', 'start_date', 'end_date']
+#        if not all([v in required_parameters for v in params.keys()]):
+        if not all([v in params for v in required_parameters]):
+            raise TypeError(
+                'Query condition should have all '
+                'required parameters ids, start_date, end_date, metrics.')
+        return params
+
+    def _build_v4_query(self, params):
+        if not isinstance(params, dict):
+            raise TypeError('query parameters must be dictionary.')
+        v3_request = self._build_v3_query(params)
+        return gav4.convert_request(**v3_request)
--- tests/datasets/test_google_analytics.py
+++ tests/datasets/test_google_analytics.py
@@ -18,19 +18,26 @@ def data():
             'yukoga-analytics-api@gcp-jp.iam.gserviceaccount.com'
         params['key_file_location'] = './gcp-jp.p12'
         ga = GoogleAnalytics(params=params)
-        ga.fetch_data()
+        query_params = {
+            'ids': 'ga:114540399',
+            'start_date': '2016-01-01',
+            'end_date': '2016-07-20',
+            'dimensions': 'ga:date,ga:hour',
+            'metrics': 'ga:sessions,ga:pageviews'
+        }
+        ga.fetch_data(query_params)
         return ga
-    except Exception:
+    except TypeError:
         warn('Failed to fetch Google Analytics data. {}'
-             .format(Exception.message))
+             .format(TypeError))
 
 
 def test_googleanalytics_instantiation(data):
     # check if data type is correct.
     assert isinstance(data, GoogleAnalytics)
     assert isinstance(data.total, pd.DataFrame)
-    assert isinstance(data.features, pd.DataFrame)
-    assert isinstance(data.target, pd.Series)
+#    assert isinstance(data.features, pd.DataFrame)
+#    assert isinstance(data.target, pd.Series)
 
 
 def test_googleanalytics_data_shape(data):
@@ -41,11 +48,11 @@ def test_googleanalytics_data_shape(data):
 
 
 def test_googleanalytics_data_summary(data):
-    expected_summry_df = pd.DataFrame([['1000', '1000', '1000', '1000'],
-                                       ['98', '24', '10', '14'],
+    expected_summary_df = pd.DataFrame([[1000, 1000, 1000, 1000],
+                                       [98, 24, 10, 14],
                                        ['20160223', '16', '1', '1'],
-                                       ['20', '66', '577', '483']])
-    expected_summry_df.index = ['count', 'unique', 'top', 'freq']
-    expected_summry_df.columns = ['date', 'hour', 'sessions', 'pageiews']
+                                       [20, 66, 577, 483]])
+    expected_summary_df.index = ['count', 'unique', 'top', 'freq']
+    expected_summary_df.columns = ['date', 'hour', 'sessions', 'pageviews']
 
-    assert data.total.describe() == expected_summry_df
+    assert data.total.describe().equals(expected_summary_df)
